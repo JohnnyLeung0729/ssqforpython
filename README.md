9198035f088c6c8abfc24cf27cecd119d8f15857
JohnnyLeung <42374235@qq.com>
2021-11-21 00:21:51 +0800

    20211120提交更新，增加接口获取开奖记录，并且完整获取2003-202111月的开奖记录，导表，整体程序1.0提交

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ssqforpython
+init 20211119 for ssq get all number
--- rdexcelforssq.py
+++ rdexcelforssq.py
@@ -2,6 +2,18 @@ import xlrd
 from xlrd import xldate_as_tuple
 import datetime
 import mysql.connector as connt
+import requests
+import json
+
+weekdict = {
+    "一":1,
+    "二":2,
+    "三":3,
+    "四":4,
+    "五":5,
+    "六":6,
+    "日":7,
+}
 
 data1=xlrd.open_workbook("/Users/liangye/Downloads/ssq.xls")
 table=data1.sheets()[0]
@@ -14,9 +26,9 @@ def import_excel(excel,conn):
         dateidstr=""
         try:
             dateidstr=fill_dateid(str(int(table.cell_value(rown,1))))
-            add_ssq(conn,dateidstr,'null',table.cell_value(rown,9),table.cell_value(rown,10),table.cell_value(rown,11),table.cell_value(rown,12),table.cell_value(rown,13),table.cell_value(rown,14),int(table.cell_value(rown,15)),'null','null','null','null','null','null','null',False)
+            add_ssq(conn,dateidstr,'null',table.cell_value(rown,9),table.cell_value(rown,10),table.cell_value(rown,11),table.cell_value(rown,12),table.cell_value(rown,13),table.cell_value(rown,14),int(table.cell_value(rown,15)),'null','null','null','null','null','null','null',0,'null','null','null')
         except Exception:
-            print('转换内容失败！不导入数据库')
+            print('convert content is fail! don`t import data to database')
         if countflag==dateidstr:
             break
     
@@ -25,20 +37,28 @@ def ssq_connect(user,pwd,host,database):
     try:
         con=connt.connect(user=user,password=pwd,host=host,database=database)
     except Exception:
-        print('数据库连接失败!')
+        print('op database is fail!')
         exit(1)
     else:
         return con
 
-def add_ssq(conn,dateid,windate,winnumber1,winnumber2,winnumber3,winnumber4,winnumber5,winnumber6,winnumber7,sellprice,wins1,price1,wins2,price2,wins3,price3,sunday):
+def add_ssq(conn,dateid,windate,winnumber1,winnumber2,winnumber3,winnumber4,winnumber5,winnumber6,winnumber7,sellprice,wins1,price1,wins2,price2,wins3,price3,sunday,sales,poolmoney,memo):
     mycursor=conn.cursor()
 
-    sqlstr="insert into number_info(dateid, windate, winnumber1, winnumber2, winnumber3, winnumber4, winnumber5, winnumber6, winnumber7, sellprice, wins1, price1, wins2, price2, wins3, price3, sunday) values ("+str(dateid)+","+str(windate)+","+str(winnumber1)+","+str(winnumber2)+","+str(winnumber3)+","+str(winnumber4)+","+str(winnumber5)+","+str(winnumber6)+","+str(winnumber7)+","+str(sellprice)+","+str(wins1)+","+str(price1)+","+str(wins2)+","+str(price2)+","+str(wins3)+","+str(price3)+","+str(sunday)+")"
-    mycursor.execute(sqlstr)
-    conn.commit()
+    sqlstr="insert into number_info(dateid, windate, winnumber1, winnumber2, winnumber3, winnumber4, winnumber5, winnumber6, winnumber7, sellprice, wins1, price1, wins2, price2, wins3, price3, sunday,sales,poolmoney,memo) values ("+str(dateid)+",'"+str(windate)+"',"+str(winnumber1)+","+str(winnumber2)+","+str(winnumber3)+","+str(winnumber4)+","+str(winnumber5)+","+str(winnumber6)+","+str(winnumber7)+","+str(sellprice)+","+str(wins1)+","+str(price1)+","+str(wins2)+","+str(price2)+","+str(wins3)+","+str(price3)+","+str(sunday)+","+str(sales)+","+str(poolmoney)+",'"+str(memo)+"')"
+    try:
+        mycursor.execute(sqlstr)
+        conn.commit()
+    except Exception:
+        print('primary key error')
     mycursor.close()
 
 def fill_dateid(dateid):
+    """
+        about abbreviation ssq period numebr fill complete
+        if number is 4 char then fill 3 char
+        if number is 5 char then fill 2 char
+    """
     longs=len(dateid)
     if longs==4:
         return '200'+dateid
@@ -47,7 +67,50 @@ def fill_dateid(dateid):
     else:
         return dateid
 
+def read_winnumberinfo_websocket(stcode,edcode):
+    """
+        http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name=ssq&issueCount=&issueStart=2021100&issueEnd=2022001&dayStart=&dayEnd=
+        这个接口每次只返回100条记录，并且是根据期号倒序返回，需要注意。
+    """
+    r=requests.get("http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name=ssq&issueCount=&issueStart="+str(stcode)+"&issueEnd="+str(edcode)+"&dayStart=&dayEnd=")
+    nums=r.text
+    num_dict=json.loads(nums)
+    return num_dict['result']
+
+def import_socket(conns,resto):
+    """
+        code    期数
+        date    开奖日期带星期
+        week    星期几
+        red     红色球，号分割6位
+        blue    蓝色球
+        sales   销售额
+        poolmoney   流转下月奖池金额
+        content 兑奖公告
+    """
+    for nr_list in resto:
+        r=split_red(nr_list["red"])
+        add_ssq(conns,nr_list["code"],split_date(nr_list["date"]),r[0],r[1],r[2],r[3],r[4],r[5],nr_list["blue"],nr_list["sales"],'null','null','null','null','null','null',return_weeknum(split_week(nr_list["date"])),nr_list["sales"],nr_list["poolmoney"],nr_list["content"])
+
+def split_red(red):
+    r=red.split(",")
+    return r
+
+def split_date(date_):
+    return date_[:10]
+
+def split_week(date_):
+    return date_[11:12]
+
+def return_weeknum(weeks):
+    return weekdict[weeks]    
+
 if __name__ == '__main__':
     conns=ssq_connect('johnny','Ly818379','localhost','ssq')
-    import_excel(table,conns)
+    # 针对2013年版本双色球excel数据表做的数据导入数据库的函数
+    # import_excel(table,conns)
+
+    #针对2021年版本福彩网往期开奖记录接口的导入数据，一次只能查询100期，注意
+    res=read_winnumberinfo_websocket('2021100','2022001')
+    import_socket(conns,res)
     conns.close()
